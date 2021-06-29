64fc52e901c782970186c3784f0ffb1def2e65c8
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2021-06-29 20:38:28 +0300

    added srver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log(1)
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const schema = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello client!",
+  },
+};
+
+const PORT = 4000;
+
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () =>
+  console.log(
+    `Server is running at http://localhost:${PORT}${server.graphqlPath}`
+  )
+);
